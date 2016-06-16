f7f15f6c58f9f45f8353c97229c1b107272a7a17
danieljauch <daniel.p.jauch@gmail.com>
2016-06-16 13:14:34 -0700

    Fixes some of the remaining issues. Almost completes the issue, Michael taking over with what's left

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -1,61 +1,53 @@
 $(document).ready(function() {
-
-    // MOBILE MENU FUNCTIONALITY
-    $(".action--open").click(function() {
-        $(".menu").addClass("menu--open");
+  // MOBILE MENU FUNCTIONALITY
+  $(".action--open").click(function() {
+    $(".menu").addClass("menu--open");
+  });
+  $(".action--close").click(function() {
+    $(".menu").removeClass("menu--open");
+  });
+  $('nav.more-nav ul').addClass('hide');
+  $(".menu-btn").click(function() {
+    $("nav.more-nav ul").toggleClass('hide');
+    $(".menu-btn").toggleClass('close');
+  });
+  // COLORBOX
+  if ($('.gallery-item').length > 0) {
+    $(".gallery-item").colorbox({
+      rel: 'gallery-item'
     });
-    $(".action--close").click(function() {
-        $(".menu").removeClass("menu--open");
-    });
-
-    $('nav.more-nav ul').addClass('hide');
-
-    $(".menu-btn").click(function() {
-        $("nav.more-nav ul").toggleClass('hide');
-        $(".menu-btn").toggleClass('close');
-    });
-
-    // COLORBOX
-    if ($('.gallery-item').length > 0) { 
-        $(".gallery-item").colorbox({rel:'gallery-item'});
-    }
-
-    if (Modernizr.mq('only all')) {
-        $('html').addClass('mq');
-    } else {
-        $('html').addClass('no-mq');
-    };
-    // PNG LOGO FALLBACK FOR OLD BROWSERS
-    if (!Modernizr.svg) {
-        $(".logo img").attr("src", "/assets/images/logos/logo.png");
-    }
-
-    // OPEN UP ALL EXTERNAL LINKS IN A NEW WINDOW
-    if (window.location.host != "app.cloudcannon.com") {
-        $('a').each(function() {
-            var a = new RegExp('/' + window.location.host + '/');
-            if (!a.test(this.href)) {
-                $(this).click(function(event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    window.open(this.href, '_blank');
-                });
-            }
+  }
+  if (Modernizr.mq('only all')) {
+    $('html').addClass('mq');
+  } else {
+    $('html').addClass('no-mq');
+  };
+  // PNG LOGO FALLBACK FOR OLD BROWSERS
+  if (!Modernizr.svg) {
+    $(".logo img").attr("src", "/assets/images/logos/logo.png");
+  }
+  // OPEN UP ALL EXTERNAL LINKS IN A NEW WINDOW
+  if (window.location.host != "app.cloudcannon.com") {
+    $('a').each(function() {
+      var a = new RegExp('/' + window.location.host + '/');
+      if (!a.test(this.href)) {
+        $(this).click(function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          window.open(this.href, '_blank');
         });
-    };
-
-    // SMOOTH ANCHOR SCROLL
-    $('a[href^="#"]').on('click', function(e) {
-        e.preventDefault();
-
-        var target = this.hash,
-            $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function() {
-            window.location.hash = target;
-        });
+      }
     });
-
+  };
+  // SMOOTH ANCHOR SCROLL
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
 });
